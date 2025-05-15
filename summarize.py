65baface9c5fd452c355c019b3d777ea97e4c556
Yitayew A <yaworkineh24@gmail.com>
2025-05-15 14:40:50 -0400

    Add new files to branch

--- summarize.py
+++ summarize.py
@@ -1,8 +1,8 @@
 import os
 import openai
 #from openai import OpenAI
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 #client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 openai.api_key = os.getenv("OPENAI_API_KEY")
