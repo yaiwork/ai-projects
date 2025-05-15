a6a1b4dc82d99377aca29568cbf1587dee9399a7
Yitayew A <yaworkineh24@gmail.com>
2025-05-15 14:20:08 -0400

    Add new files to branch

--- .ipynb_checkpoints/summarize-checkpoint.py
+++ .ipynb_checkpoints/summarize-checkpoint.py
@@ -1,9 +1,9 @@
 import os
-#from openai import OpenAI
-from dotenv import load_dotenv
 import openai
+#from openai import OpenAI
+# from dotenv import load_dotenv
+# load_dotenv()
 
-load_dotenv()
 #client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 openai.api_key = os.getenv("OPENAI_API_KEY")
 def summarize_text(text, model="gpt-4"): # gpt-3.5-turbo
